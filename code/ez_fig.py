ff2af5e28f15a4ea5ea7873b2a4e87bd13cf77f7
myself <ryan950422020060422nayr@gmail.com>
2026-08-07 12:00:58 +0800

    Update from IAA

--- code/ez_fig.py
+++ code/ez_fig.py
@@ -1,31 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from scipy.stats import kstest
 
 llr_dens = np.load('/data/TARA/code/llr_dens.npy', allow_pickle=True)
 xgb_dens = np.load('/data/TARA/code/xgb_dens.npy', allow_pickle=True)
 STRM_dens = np.load('/data/TARA/code/STRM_dens.npy', allow_pickle=True)
 
 fig, ax = plt.subplots(2,2, figsize=(8, 5))
+d, p = kstest(xgb_dens[0][xgb_dens[1] == 'barred'], xgb_dens[0][xgb_dens[1] == 'unbarred'])
 ax[0,0].hist(xgb_dens[0][xgb_dens[1] == 'barred'], bins=20, label='barred', color='green', alpha=0.5, density=True)
 ax[0,0].hist(xgb_dens[0][xgb_dens[1] == 'unbarred'], bins=20, label='unbarred', color='red', alpha=0.5, density=True)
+text_placeholder = mpatches.Rectangle((0, 0), 1, 1, fill=False, edgecolor='none', visible = False)
+handles, labels = ax[0,0].get_legend_handles_labels()
+handles.extend([text_placeholder])
+labels.extend([f'K-S Test p-value: {p:.4f}'])
 ax[0,0].set_title('XGB Density Distribution')
 ax[0,0].set_xlabel('Density (5th nearest neighbor)')
 ax[0,0].set_ylabel('Frequency')
-ax[0,0].legend(loc='upper right')
+ax[0,0].legend(handles = handles, labels = labels, loc='upper right', fontsize=8)
 
+d, p = kstest(llr_dens[0][llr_dens[1] == 'barred'], llr_dens[0][llr_dens[1] == 'unbarred'])
 ax[0,1].hist(llr_dens[0][llr_dens[1] == 'barred'], bins=20, label='barred', color='green', alpha=0.5, density=True)
 ax[0,1].hist(llr_dens[0][llr_dens[1] == 'unbarred'], bins=20, label='unbarred', color='red', alpha=0.5, density=True)
+text_placeholder = mpatches.Rectangle((0, 0), 1, 1, fill=False, edgecolor='none', visible = False)
+handles, labels = ax[0,1].get_legend_handles_labels()
+handles.extend([text_placeholder])
+labels.extend([f'K-S Test p-value: {p:.4f}'])
 ax[0,1].set_title('LLR Density Distribution')
 ax[0,1].set_xlabel('Density (5th nearest neighbor)')
 ax[0,1].set_ylabel('Frequency')
-ax[0,1].legend(loc='upper right')
+ax[0,1].legend(handles = handles, labels = labels, loc='upper right', fontsize=8)
 
+d, p = kstest(STRM_dens[0][STRM_dens[1] == 'barred'], STRM_dens[0][STRM_dens[1] == 'unbarred'])
 ax[1,0].hist(STRM_dens[0][STRM_dens[1] == 'barred'], bins=20, label='barred', color='green', alpha=0.5, density=True)
 ax[1,0].hist(STRM_dens[0][STRM_dens[1] == 'unbarred'], bins=20, label='unbarred', color='red', alpha=0.5, density=True)
+text_placeholder = mpatches.Rectangle((0, 0), 1, 1, fill=False, edgecolor='none', visible = False)
+handles, labels = ax[1,0].get_legend_handles_labels()
+handles.extend([text_placeholder])
+labels.extend([f'K-S Test p-value: {p:.4f}'])
 ax[1,0].set_title('STRM Density Distribution')
 ax[1,0].set_xlabel('Density (5th nearest neighbor)')
 ax[1,0].set_ylabel('Frequency')
-ax[1,0].legend(loc='upper right')
+ax[1,0].legend(handles = handles, labels = labels, loc='upper right', fontsize=8)
 
 ax[1,1].hist(xgb_dens[0][0], bins=20, label='XGB density distribution', color='green', alpha=0.3, density=True)
 ax[1,1].hist(llr_dens[0][0], bins=20, label='LLR density distribution', color='blue', alpha=0.3, density=True)
@@ -33,7 +50,7 @@ ax[1,1].hist(STRM_dens[0], bins=20, label='STRM density distribution', color='re
 ax[1,1].set_xlabel('Density (5th nearest neighbor)')
 ax[1,1].set_ylabel('Frequency')
 ax[1,1].set_title('Histogram of Density Values')
-ax[1,1].legend(loc='upper right')
+ax[1,1].legend(loc='upper right', fontsize=8)
 
 plt.tight_layout()
 plt.savefig(f'/data/TARA/fig/density_histogram.png', dpi=300)
